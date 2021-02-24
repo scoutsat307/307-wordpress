6abc3058ccbf63f933e5ea6b021370154d88b985
Michael Thomas <michaelhthomas@outlook.com>
2021-02-23 22:00:36 -0500

    Add nix shell dev environment

---
+++ shell.nix
@@ -0,0 +1,14 @@
+{ pkgs ? import <nixpkgs> {} }:
+  pkgs.mkShell {
+    # nativeBuildInputs is usually what you want -- tools you need to run
+    nativeBuildInputs = with pkgs; [ 
+      # Node Deps
+      nodejs
+      nodePackages.npm
+      nodePackages.yarn
+
+      # Php Deps
+      php74
+      php74Packages.composer
+    ];
+}
