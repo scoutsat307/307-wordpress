7d463a88c035c49a59d67a41fb6e5b67a8360c21
Michael Thomas <michaelhthomas@outlook.com>
2021-02-24 22:27:48 -0500

    Make archive page functional

--- theme/archive.php
+++ theme/archive.php
@@ -26,7 +26,7 @@ if ( is_day() ) {
 } elseif ( is_year() ) {
 	$context['title'] = 'Archive: ' . get_the_date( 'Y' );
 } elseif ( is_tag() ) {
-	$context['title'] = single_tag_title( '', false );
+	$context['title'] = '#' . single_tag_title( '', false );
 } elseif ( is_category() ) {
 	$context['title'] = single_cat_title( '', false );
 	array_unshift( $templates, 'archive-' . get_query_var( 'cat' ) . '.twig' );
